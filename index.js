d9fc0bc998c9b0b1e224229b6c9874e8339008ed
sw28d023 <72960479+sw28d023@users.noreply.github.com>
2020-10-16 14:52:50 +0800

    Update index.js

--- index.js
+++ index.js
@@ -1,17 +1,36 @@
-<!DOCTYPE html>
-<html>
-  <head>
-    <meta charset="UTF-8" />
-    <title>Hello World</title>
-    <script src="https://unpkg.com/react@16/umd/react.development.js"></script>
-    <script src="https://unpkg.com/react-dom@16/umd/react-dom.development.js"></script>
-    
-    <!-- Don't use this in production: -->
-    <script src="https://unpkg.com/@babel/standalone/babel.min.js"></script>
-  </head>
-  <body>
-    <div id="root"></div>
-    <script type="text/babel" src="index.js" > </script>
+function M(){ //function component. props-gui
+    return <h1>swvwev</h1>;
+}
+function M1(props){ //function component. props-toi
+return <h1>{props.ner}</h1>;
+}
+function Mend2(props){ //function component. props-toi olon html renderlen
+    return(
+        <div>
+            <h1>{props.ner}</h1>
+            <h1>{props.age}</h1>
+        </div>
+    )
+}
+function EtsegComp(props){
+    return(
+        <div>
+            <M/>
+            <M1 ner={props.ner}/>
+            <Mend2 ner={props.ner} age={props.age}/>
+            <table>
+                <tr><td>{props.angi[0]}</td></tr> 
+                <tr><td>{props.angi[1]}</td></tr> 
+                <tr><td> {props.angi[2]}</td></tr> 
+            </table>
+        </div>
+    )
+}
 
-  </body>
-</html>
+
+ReactDOM.render(
+    // <M/>,
+    // <M1 ner="hello bat"/>,
+    // <Mend2 ner="dorj" age="12"/>,
+    <EtsegComp ner="jems" age="20" angi={['пх','мс','нббмс']}/>,
+    document.getElementById('root'));
